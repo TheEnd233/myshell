c47a715da940d77a676cf49462ca49c4e8c28bd2
TheEnd233 <963508700@qq.com>
2021-08-22 15:59:13 +0800

    update install_moespeed.sh

--- install_moespeed.sh
+++ install_moespeed.sh
@@ -1,6 +1,6 @@
-sudo wget -O "/root/speedtest" "https://raw.githubusercontent.com/TheEnd233/vps_auto_speedtest/master/speedtest" --no-check-certificate -T 30 -t 5 -d
+sudo wget -O "/root/speedtest" "https://raw.githubusercontent.com/TheEnd233/myshell/master/speedtest" --no-check-certificate -T 30 -t 5 -d
 sudo chmod +x "/root/speedtest"
 sudo chmod 777 "/root/speedtest"
-sudo wget -O "/root/moespeed.sh" "https://raw.githubusercontent.com/TheEnd233/vps_auto_speedtest/master/moespeed.sh" --no-check-certificate -T 30 -t 5 -d
+sudo wget -O "/root/moespeed.sh" "https://raw.githubusercontent.com/TheEnd233/myshell/master/install_moespeed.sh" --no-check-certificate -T 30 -t 5 -d
 sudo chmod +x "/root/moespeed.sh"
 sudo chmod 777 "/root/moespeed.sh"
