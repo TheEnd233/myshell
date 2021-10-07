7f191ef9813e6cebce919e799bd369a36b6f6df3
泡泡大鱼 <963508700@qq.com>
2021-10-07 15:20:57 +0800

    Update install_python3.sh

--- install_python3.sh
+++ install_python3.sh
@@ -3,7 +3,7 @@ yum install -y zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel r
 wget https://www.python.org/ftp/python/3.8.1/Python-3.8.1.tgz
 tar -zxvf Python-3.8.1.tgz  
 cd Python-3.8.1
-./configure prefix=/usr/local/python3
+./configure prefix=/usr/local/python38
 make && make install
-ln -s /usr/local/python3/bin/python3.8 /usr/bin/python3
-ln -s /usr/local/python3/bin/pip3.8 /usr/bin/pip3
+ln -s /usr/local/python3/bin/python3.8 /usr/bin/python38
+ln -s /usr/local/python3/bin/pip3.8 /usr/bin/pip38
