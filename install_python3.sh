75bb3b6b19ed711c8f601f1aa007201cdc0cee8d
泡泡大鱼 <963508700@qq.com>
2021-10-07 15:50:24 +0800

    Update install_python3.sh

--- install_python3.sh
+++ install_python3.sh
@@ -5,5 +5,5 @@ tar -zxvf Python-3.8.1.tgz
 cd Python-3.8.1
 ./configure prefix=/usr/local/python38
 make && make install
-ln -s /usr/local/python3/bin/python3.8 /usr/bin/python38
-ln -s /usr/local/python3/bin/pip3.8 /usr/bin/pip38
+ln -s /usr/local/python38/bin/python3.8 /usr/bin/python38
+ln -s /usr/local/python38/bin/pip3.8 /usr/bin/pip38
